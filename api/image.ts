6309acab371e923aa2a7c69f9d36b587423d3fc8
erinskieasy <xx876xx@gmail.com>
2026-05-26 18:15:12 -0500

    fix: switch image generation from dall-e-2 to dall-e-3 at 1024x1024

--- api/image.ts
+++ api/image.ts
@@ -40,10 +40,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     
     // Generate image
     const response = await openai.images.generate({
-      model: "dall-e-2",
+      model: "dall-e-3",
       prompt: themePrompt,
       n: 1,
-      size: "512x512",
+      size: "1024x1024",
     });
 
     const imageUrl = response.data?.[0]?.url;
